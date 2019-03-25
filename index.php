9a7b6bbb3c5352bfe72458df6430c3f6d16b9434
Luis Ramos <luisenriqueramos1977@gmail.com>
2019-03-25 07:28:45 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+
+$title = "Home";
+
+$content = '<img src="images/jabones/industrial.jpg" class="imgLeft" />
+
+<title>Jabones de Avena</title>
+
+<p> 
+
+El jabón industrial de avena es un producto especialmente diseñado para brindar muy buena espuma, dureza  y fragancia a un costo aceptable, el cual puede ser hasta un 50 % del costo de un jabón del mismo tipo en el distribuidor final. 
+
+Su color puede variar del blanco al marfil claro. Se puede utilizar tanto para baño, como para el cuidado del rostro y de las manos. 
+
+La presentación del jabón es en empaque de papel glase, o sin empaque. 
+
+Los ingredientes del jabón son grasa animal y vegetal, soda cáustica, agua, avena en hojuelas y fragancia. 
+
+
+
+</p>
+
+<img src="images/virutas/viruta blanca.jpg" class="imgRight" />
+
+<title> Virutas de Jab[on</title>
+
+
+
+<p> 
+
+El jabón industrial de avena es un producto especialmente diseñado para brindar muy buena espuma, dureza  y fragancia a un costo aceptable, el cual puede ser hasta un 50 % del costo de un jabón del mismo tipo en el distribuidor final. 
+
+Su color puede variar del blanco al marfil claro. Se puede utilizar tanto para baño, como para el cuidado del rostro y de las manos. 
+
+La presentación del jabón es en empaque de papel glase, o sin empaque. 
+
+Los ingredientes del jabón son grasa animal y vegetal, soda cáustica, agua, avena en hojuelas y fragancia. 
+
+
+</p>
+
+<img src="images/desodorante/desodoranteBolsa.jpg" class="imgLeft" />
+
+<title> Desodorante</title>
+
+
+<p>
+
+Hemos desarrollado este desodorante desodorante en spray, el cual brinda la protección básica de antitranspirante. 
+Se tiene en tres presentaciones neutro, caballero y damas. 
+
+
+Ingredientes:
+
+Agua destilada
+
+Clohidróxido de Aluminio
+
+Fragancia (presentaciones con olor)
+
+Con la compra de este producto, se puede optar por la compra de la recarga, con la cual se optiene un ahorro de más del 50 %. 
+
+</p>';
+
+include 'template.php';
+
+
+?>
+
---
+++ template.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+<title>?php echo $title; ?</title>
+	<link rel="stylesheet" type="text/css" href="styles/stylesheet.css"></link>
+</head>
+
+<body>
+	<div id="wrapper">
+			<div id="banner">
+				</div>
+			 <nav id="navigation">
+					<ul id="nav">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="#">Jabones</a></li>
+						<li><a href="#">Virutas</a></li>
+						<li><a href="#">Desodorantes</a></li>
+					</ul>
+			</nav>
+				<div id="content_area">
+					<?php echo $content; ?>
+				</div>
+				
+				<div id="sidebar">
+			
+				</div>
+			
+				<footer>
+				<p> "Todos los Derechos Reservado"</p>
+				</footer>
+		</div>
+    </body>
+    
+</html>
